180c25b050fa601245d429f709736a005ac44e33
Alexandre <alexandre.maciele@gmail.com>
2025-08-31 12:17:54 -0300

    feature: criacao da entidade produto; adicao de editor config (#2)

--- src/MataAtlanticaV2.Domain/Entidades/Produto.cs
+++ src/MataAtlanticaV2.Domain/Entidades/Produto.cs
@@ -6,9 +6,17 @@ public class Produto
 
     public string Nome { get; set; }
 
+    public string Descricao { get; set; }
+
     public string Categoria { get; set; }
 
     public string Marca { get; set; }
+
+    public float Preco { get; set; }
+
+    public int Quantidade { get; set; }
+
+    public List<ItemProduto> Items { get; set; } = new();
 
     public DateTime CriadoEm { get; set; }
 
